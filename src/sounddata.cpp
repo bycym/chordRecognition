03388ce36a1bbf3fe661b91977ef6665613992f2
arcsibo <arcsibo@gmail.com>
2014-10-20 18:38:38 +0200

    libs problem, hit me! :D

---
+++ src/sounddata.cpp
@@ -0,0 +1,5 @@
+#include "sounddata.h"
+
+SoundData::SoundData()
+{
+}
---
+++ src/sounddata.h
@@ -0,0 +1,10 @@
+#ifndef SOUNDDATA_H
+#define SOUNDDATA_H
+
+class SoundData
+{
+public:
+    SoundData();
+};
+
+#endif // SOUNDDATA_H
