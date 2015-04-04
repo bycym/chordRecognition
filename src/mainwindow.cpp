c0c19cd5048514ec0297b9741132c73d67b19ffa
arcsibo <arcsibo@gmail.com>
2015-04-04 10:46:52 +0200

    mainwindows test stuff

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -36,11 +36,6 @@ void MainWindow::on_pushButton_clicked()
     getFeatures(sndData_);
     info();
 
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(-1, 1);
-    for(int i = 0; i<10;i++)
-        std::cout << dist(mt) << std::endl;
 
 }
 
